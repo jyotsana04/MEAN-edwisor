6651cd1a8e506d5b84fece8e564d4f5aeec02257
jyotsana <jyotsana.negi04@gmail.com>
2019-09-16 18:52:53 +0530

    second-commit

---
+++ apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "My Practice application",
+  "version": "0.0.1",
+  "description": "Document for the myPrac app",
+  "title": "myPrac API",
+  "url": "http://localhost:3000",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-09-16T12:55:45.862Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
